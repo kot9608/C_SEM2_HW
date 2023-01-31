b116857b959a67faa4b897de79da3b451fb10e0c
Kirill <Kot9608@mail.ru>
2023-01-31 16:21:52 +0500

    HW2 - TASK10

---
+++ HW2 - TASK10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int num1 = int.Parse(Console.ReadLine());
+if ((num1/100==0) ||(num1/100==10))
+  {
+  Console.WriteLine ("Введено не трёхзначное число");
+  return;
+  }
+int num2=num1/10;
+int result=num2%10;
+Console.WriteLine (result);
