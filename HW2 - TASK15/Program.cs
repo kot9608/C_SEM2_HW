f51e136574440919aecd34a2d56ae5eccf5fb427
Kirill <Kot9608@mail.ru>
2023-01-31 17:04:03 +0500

    HW2 - TASK15

---
+++ HW2 - TASK15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+if (num1<1 || num1>7)
+  {
+  Console.WriteLine ("Введён некорректный день недели");
+  return;
+  }
+else if (num1<6) {
+  Console.WriteLine("Нет");
+}
+else  {
+  Console.WriteLine("Да");
+}
